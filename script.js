329bd191a0e2de82c8fef5b69193d1257b5219dd
Sreelekha6568 <sreelekha0419@gmail.com>
2024-12-19 11:06:57 +0530

    Add files via upload
    Task2

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-function Myfun(){
-    alert(Date())
+function Addnum(){
+    let num1=document.getElementById('firstname').value
+    let num2=document.getElementById('secondname').value
+num1=parseInt(num1)
+num2=parseInt(num2)
+let h1=document.getElementById("result")
+res=num1+num2
+h1.innerHTML="Result:"+res
 }
+function Subnum(){
+    let num1=document.getElementById('firstname').value
+    let num2=document.getElementById('secondname').value
+num1=parseInt(num1)
+num2=parseInt(num2)
+let h1=document.getElementById("result")
+res=num1-num2
+h1.innerHTML="Result:"+res
+}
+function Mulnum(){
+    let num1=document.getElementById('firstname').value
+    let num2=document.getElementById('secondname').value
+num1=parseInt(num1)
+num2=parseInt(num2)
+let h1=document.getElementById("result")
+res=num1*num2
+h1.innerHTML="Result:"+res
+}
+function Divnum(){
+    let num1=document.getElementById('firstname').value
+    let num2=document.getElementById('secondname').value
+num1=parseInt(num1)
+num2=parseInt(num2)
+let h1=document.getElementById("result")
+res=num1/num2
+h1.innerHTML="Result:"+res
+}
+function Pownum(){
+    let num1=document.getElementById('firstname').value
+    let num2=document.getElementById('secondname').value
+num1=parseInt(num1)
+num2=parseInt(num2)
+let h1=document.getElementById("result")
+res=num1**num2
+h1.innerHTML="Result:"+res
+}
+
